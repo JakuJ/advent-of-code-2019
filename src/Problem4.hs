ada567fce80e4b61286c61e79d5b42922ed53ed6
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2019-12-07 03:43:43 +0100

    Problem 4 refactoring

--- src/Problem4.hs
+++ src/Problem4.hs
@@ -2,42 +2,36 @@ module Problem4 (problem4) where
 
 import           Problem       (problem)
 
-import           Control.Monad (guard)
+import           Data.Function (on)
 import           Data.List     (group)
 
 lower, upper :: Int
 (lower, upper) = (136760, 595730)
 
 twoAdjacent :: String -> Bool
-twoAdjacent []       = False
-twoAdjacent [x]      = False
-twoAdjacent (x:y:xs) = x == y || twoAdjacent (y : xs)
+twoAdjacent = any ((>= 2) . length) . group
 
 nonDecreasing :: String -> Bool
 nonDecreasing str = and $ zipWith (>=) (tail str) str
 
-checkRules :: Int -> Bool
-checkRules i = and [i >= lower, i <= upper, twoAdjacent str, nonDecreasing str]
-    where
-        str = show i
+checkRules :: String -> Bool
+checkRules = and . ([twoAdjacent, nonDecreasing] <*>) . pure
 
 -- PART 1
 
-bruteforce :: [Int]
-bruteforce = do
-    pass <- [lower .. upper]
-    guard $ checkRules pass
-    return pass
+bruteforce :: [String]
+bruteforce = [show pass | pass <- [lower .. upper], checkRules (show pass)]
 
-part1 :: IO Int
-part1 = return $ length bruteforce
+part1 :: Int
+part1 = length bruteforce
 
 -- PART 2
 
-onlyTwoAdjacent :: Int -> Bool
-onlyTwoAdjacent = any ((== 2) . length) . group . show
+onlyTwoAdjacent :: String -> Bool
+onlyTwoAdjacent = any ((== 2) . length) . group
 
-part2 :: IO Int
-part2 = return . length . filter onlyTwoAdjacent $ bruteforce
+part2 :: Int
+part2 = length . filter onlyTwoAdjacent $ bruteforce
 
-problem4 = problem 4 part1 part2
+problem4 :: IO ()
+problem4 = (problem 4 `on` return) part1 part2
